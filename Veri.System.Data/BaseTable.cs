4b5e71bdfe62381ac6736a1b8e9db873aaf0cf17
Eralp Erakalin <eralpe@veriland.co.uk>
2020-12-20 21:41:47 +0000

    IBaseTable moved to new file

--- Veri.System.Data/BaseTable.cs
+++ Veri.System.Data/BaseTable.cs
@@ -7,21 +7,6 @@ using System.Text;
 namespace Veri.System.Data
 {
 
-    public interface IBaseTable
-    {
-        ICacheContext Cache { get; set; }
-
-        void Save(int ttl = -1);
-
-        bool Validate();
-
-        void Delete();
-
-        void Insert(int ttl = -1);
-
-        SysRequestLog RequestLog(string operation, string method = "GET", string identity = "N/A", string message = "success");
-    }
-   
     public abstract class BaseTable : IBaseTable
     {
         [IgnoreDataMember]
